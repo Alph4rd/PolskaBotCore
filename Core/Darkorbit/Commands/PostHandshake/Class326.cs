e1410b5748793b1abd1d91b896ddc6e968c1a9fc
Tanoshiizo <15257735+Alph4rd@users.noreply.github.com>
2019-04-09 05:20:45 -0300

    Update Class326.cs

--- Core/Darkorbit/Commands/PostHandshake/Class326.cs
+++ Core/Darkorbit/Commands/PostHandshake/Class326.cs
@@ -12,23 +12,27 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
         public const ushort ID = 18881;
 
         public bool Activated { get; private set; }
-        public ushort var_2744 { get; private set; }
-        public string var_2555 { get; private set; }
+        public ushort modifier { get; private set; }
+        public byte[] var_2555 { get; private set; }
         public int Attribute { get; private set; }
-        public int name_125 { get; private set; }
+        public int userId { get; private set; }
         public int Count { get; private set; }
 
         public Class326(EndianBinaryReader reader)
         {
-            var_2555 = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
+            Count = reader.ReadInt32();
+            Count = Count << 4 | Count >> 28;
+            short strlen = reader.ReadInt16();
+            var_2555 = reader.ReadBytes(strlen);
+            
             Attribute = reader.ReadInt32();
             Attribute = Attribute >> 13 | Attribute << 19;
-            var_2744 = reader.ReadUInt16();
+            userId = reader.ReadInt32();
+            userId = userId >> 7 | userId << 25;
+            modifier = reader.ReadUInt16();
             Activated = reader.ReadBoolean();
-            name_125 = reader.ReadInt32();
-            name_125 = name_125 << 13 | name_125 >> 19;
-            Count = reader.ReadInt32();
-            Count = Count << 13 | Count >> 19;
+
+
         }
     }
 }
