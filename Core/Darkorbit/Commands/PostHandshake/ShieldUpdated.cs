e26e986725597ef50cf26c19e255324dfc733c3e
Tanoshiizo <15257735+Alph4rd@users.noreply.github.com>
2019-04-09 16:45:20 -0300

    Update ShieldUpdated.cs

--- Core/Darkorbit/Commands/PostHandshake/ShieldUpdated.cs
+++ Core/Darkorbit/Commands/PostHandshake/ShieldUpdated.cs
@@ -9,17 +9,18 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 {
     class ShieldUpdated : Command
     {
-        public const ushort ID = 21535;
+        public const ushort ID = 5659;
 
         public int MaxShield { get; private set; }  //name_101
         public int Shield { get; private set; }     //var_752
 
         public ShieldUpdated(EndianBinaryReader reader)
         {
-            Shield = reader.ReadInt32();
-            Shield = (int)((uint)Shield >> 14 | (uint)Shield << 18);
             MaxShield = reader.ReadInt32();
-            MaxShield = (int)((uint)MaxShield >> 2 | (uint)MaxShield << 30);
+            MaxShield = (int)((uint)MaxShield << 10 | (uint)MaxShield >> 22);
+            reader.ReadInt16();
+            Shield = reader.ReadInt32();
+            Shield = (int)((uint)Shield << 13 | (uint)Shield >> 19);
             reader.ReadInt16();
         }
     }
