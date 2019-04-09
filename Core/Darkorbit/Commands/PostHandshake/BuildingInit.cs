699350c32f052432c08f937698aa9ad1644e95b7
Tanoshiizo <15257735+Alph4rd@users.noreply.github.com>
2019-04-09 05:20:36 -0300

    Update BuildingInit.cs

--- Core/Darkorbit/Commands/PostHandshake/BuildingInit.cs
+++ Core/Darkorbit/Commands/PostHandshake/BuildingInit.cs
@@ -9,9 +9,9 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 {
     class BuildingInit : Command
     {
-        public const ushort ID = 7761;
+        public const ushort ID = 21117;
 
-        public string name_138 { get; private set; }        // name_138
+        public string ClanTag { get; private set; }        // ClanTag
         public int var_3377 { get; private set; }           // var_3377
         public int BuildingID { get; private set; }         // assetId
         public int AssetType { get; private set; }
@@ -31,45 +31,42 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 
         public BuildingInit(EndianBinaryReader reader)
         {
-            BuildingID = reader.ReadInt32();
-            BuildingID = (int)((uint)BuildingID >> 2 | (uint)BuildingID << 30);
-            X = reader.ReadInt32();
-            X = (int)((uint)X << 8 | (uint)X >> 24);
-            name_138 = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
+            //class_940 begin read
             reader.ReadUInt16();
-            var_1531 = reader.ReadBoolean();
-            var_984 = reader.ReadBoolean();
-            name_48 = reader.ReadInt32();
-            name_48 = (int)((uint)name_48 << 2 | (uint)name_48 >> 30);
-            FactionID = reader.ReadInt32();
-            FactionID = (int)((uint)FactionID >> 13 | (uint)FactionID << 19);
+            reader.ReadUInt16();
+            reader.ReadUInt16();
+            X = reader.ReadInt32();
+            X = (int)((uint)X >> 12 | (uint)X << 20);
+
             int length = reader.ReadInt32();
             if (length > 0)
             {
                 for (int i = 0; i < length; i++)
                 {
-                    reader.ReadInt16();
+                    reader.ReadInt32();
                     Class326 class326 = new Class326(reader);
                 }
             }
-            name_158 = reader.ReadInt32();
-            name_158 = (int)((uint)name_158 >> 11 | (uint)name_158 << 21);
-            var_3377 = reader.ReadInt32();
-            var_3377 = (int)((uint)var_3377 >> 6 | (uint)var_3377 << 26);
-            //class_455 begin read
-            reader.ReadUInt16();
-            AssetType = reader.ReadUInt16();
-            //class_455 end read
             Y = reader.ReadInt32();
-            Y = (int)((uint)Y >> 10 | (uint)Y << 22);
-            //class_940 begin read
-            reader.ReadUInt16();
-            reader.ReadUInt16();
-            int type = reader.ReadUInt16();
-            //class_940 end read
+            Y = (int)((uint)Y >> 15 | (uint)Y << 17);
+            var_1531 = reader.ReadBoolean();
+            BuildingID = reader.ReadInt32();
+            BuildingID = (int)((uint)BuildingID >> 7 | (uint)BuildingID << 25);
+            ClanTag = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
             Name = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
             var_4991 = reader.ReadBoolean();
+            name_158 = reader.ReadInt32();
+            name_158 = (int)((uint)name_158 >> 11 | (uint)name_158 << 21);
             var_3562 = reader.ReadBoolean();
+            FactionID = reader.ReadInt32();
+            FactionID = (int)((uint)FactionID << 14 | (uint)FactionID >> 18);
+            reader.ReadUInt16();
+            int type = reader.ReadUInt16();
+            name_48 = reader.ReadInt32();
+            name_48 = (int)((uint)name_48 >> 15 | (uint)name_48 << 17);
+            var_3377 = reader.ReadInt32();
+            var_3377 = (int)((uint)var_3377 >> 6 | (uint)var_3377 << 26);
+            //var_984 = reader.ReadBoolean();
         }
     }
 }
